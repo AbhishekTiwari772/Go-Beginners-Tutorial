52904105225c7f4a6e34a53785ca734c56fb9ba3
Abhishek Tiwari <84613703+AbhishekTiwari772@users.noreply.github.com>
2021-05-30 21:47:04 +0530

    Create 1.BasicSyntax.go

---
+++ 1.BasicSyntax.go
@@ -0,0 +1,23 @@
+/*
+This first file of GoLang tutorial tells the basic synax of a Go program. A basic Go program has the following syntax.
+*/
+
+package main
+
+import(
+  "fmt"
+)
+
+func main(){
+  fmt.Println("Hello Golang") 
+}
+
+/*
+The first line of every Go program is a package declaration. All our code of go is kept in that package.
+
+The second line of code helps import other packages to use in our program for different purposes. Every step in a go program needs a package. One of the common pacages in Golang is "fmt".
+
+The third line present there is the declaration of the main function, the place in code from where the main execution of program starts.
+
+
+*/
